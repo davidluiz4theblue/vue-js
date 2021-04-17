16d687c344037b6966b1a475eb4fc1cba109498c
DESKTOP-R0C93DG\ASUS <ly.hoang@kyanon.digital>
2021-04-17 21:37:10 +0700

    Setup src cli & demo vue lifecyle hooks

---
+++ src/components/LifeCycle.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="LifeCycle Hooks">
+    <h1>{{ title }}</h1>
+    <input type="text" :disabled="disabled" v-model="placeholder" />
+    <h2>{{ placeholder }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Hello World",
+      placeholder: "Welcome to VueJS",
+      disabled: false,
+    };
+  },
+  beforeCreate() {
+    console.log("beforeCreate");
+  },
+  created() {
+    console.log("created");
+  },
+  beforeMount() {
+    console.log("beforeMount");
+  },
+  mounted() {
+    console.log("mounted");
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate");
+  },
+  updated() {
+    console.log("updated");
+  },
+  beforeDestroy() {
+    console.log("beforeDestroy");
+  },
+  destroyed() {
+    console.log("destroyed");
+  },
+};
+</script>
+
+<style>
+input {
+  padding: 1rem .5rem;
+  font-size: 1.5rem;
+}
+</style>
